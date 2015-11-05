1ca7d0e4a3b0ad81fc1aa0153d1f04cc3cc0e931
Ruslan Loboda <ruslan.iuko777@mail.ru>
2015-11-05 02:23:33 +0200

    2:23

--- js/index.js
+++ js/index.js
@@ -1,23 +1,32 @@
-var date,day,month,year;
+var date,day,month,year,ctx;
 
 $(document).ready(function(){
     getDate();
     $("#adult").click(function(){ 
       $("#main_page").load('forms.html #adult_form');
+      $("#canvas").mouseover(drawSign());
+      
     });
     $("#child").click(function(){
       $("#main_page").load('forms.html #child_form');
     });
-    $("#back").click(function(){
-      $("#main_page").load('index.html #main_content');
-    });
-    $("#back1").click(function(){
-      $("#main_page").load('index.html #main_content');
-    });
     
-    
-    //console.log(date);
+
 });
+function drawSign(){
+    var canvas= document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+    
+    var x = ctx.offSetLeft;
+    var y = ctx.offSetTop;
+    
+    console.log(x+' '+y);
+//    ctx.beginPath();
+//    ctx.moveTo(2,1);
+//    ctx.lineTo(3,2);
+//    ctx.stroke();
+}
+
 function getDate(){
    date = new Date();
    day = date.getDate();
